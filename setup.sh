45c833a48fec94db9543dad3c9e671fff7e95892
kohlil <kohlil@miamioh.edu>
2025-05-06 22:44:02 -0400

    fix: use pip instead of conda

--- setup.sh
+++ setup.sh
@@ -2,8 +2,7 @@
 
 # Install dependencies
 echo "\n##############    Installing Dependencies    ##############\n"
-conda install -y numpy scikit-learn hyperopt tensorboard matplotlib geopandas osmnx rasterio torch-tb-profiler
-python -m pip install torch torchvision torchaudio
+python -m pip install torch torchvision torchaudio numpy scikit-learn hyperopt tensorboard matplotlib geopandas osmnx rasterio torch-tb-profiler
 
 # Download train / test data
 echo "\n\n##############    Downloading Dataset    ##############\n"
